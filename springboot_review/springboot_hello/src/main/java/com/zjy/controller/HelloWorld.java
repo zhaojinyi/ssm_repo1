e4d7b350886d15840d88b5a8955575e387ca0161
赵金毅 <1065384537@qq.com>
2019-05-08 14:23:21 +0800

    springboot复习： springboot-helloworld

---
+++ springboot_review/springboot_hello/src/main/java/com/zjy/controller/HelloWorld.java
@@ -0,0 +1,47 @@
+package com.zjy.controller;////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//            佛祖保佑       永不宕机     永无BUG                    	  //
+////////////////////////////////////////////////////////////////////
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther: 无聊的人
+ * @Date: 2019/5/8
+ * @Description: com.zjy.controller
+ */
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Map<String, Object> showHelloWorld() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "Hello World");
+        return map;
+    }
+
+}
